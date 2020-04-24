c71370d07b5d3dd6251ba6c4022e1fbd962e807b
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-04-24 11:32:07 +0300

    Select Btns + addMovieHandler func

---
+++ assets/scripts/objects.js
@@ -0,0 +1,33 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+
+
+const addMovieHandler = () =>{
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    if( title.trim() === '' ||
+        extraName.trim() === '' ||
+        extraValue.trim() === '')
+    {
+        return;
+    }
+
+    const newMovie = {
+        info: {
+            title,
+            [extraName]: extraValue
+        },
+        id: Math.floor(((Math.random()*100000) + 1))
+    };
+
+    movies.push(newMovie);
+    console.log(movies);
+
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
